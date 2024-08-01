f74a87725998f70cf5c80b948dc47ff1dcc8dc0f
Kirtika Jain <jain.kirtika08@gmail.com>
2024-08-02 00:28:34 +0530

    feat: modified getall tasks logic.

--- backend/services/todo.service.js
+++ backend/services/todo.service.js
@@ -1,61 +1,47 @@
-const db = require( "../models/index.js");
-const { ServerError,ValidationError } = require ("../errors/custom.errors.js");
+const db = require("../models/index.js");
+const { ServerError, ValidationError } = require("../errors/custom.errors.js");
 const { send } = require("process");
+const { query } = require("express");
 
 class Todolist {
-    static async addTask(taskData) {
-      console.log("taskData", taskData);
-        try {
-          const newTask = await db.Todo.create(taskData);
-          return newTask;
-        } catch (error) {
-          if (error.name === 'SequelizeValidationError') {
-            throw new ValidationError('Invalid task data');
-          }
-          console.log("err", error)
-          throw new ServerError('Error adding task');
-        }
+  static async addTask(taskData) {
+    console.log("taskData", taskData);
+    try {
+      const newTask = await db.Todo.create(taskData);
+      return newTask;
+    } catch (error) {
+      if (error.name === "SequelizeValidationError") {
+        throw new ValidationError("Invalid task data");
       }
-      static  async getAll(res,req){
-        const status = req.query.completed;
-        console.log(typeof(status));
-        if (status == "false"){
-          try {
-            const uncompletedtodo = await db.Todo.findAll({
-              where: {
-                completed: false
-              }
-            });
-            const ans = JSON.parse(JSON.stringify(uncompletedtodo));
-            return ans;
-        } catch(error){
-          console.log("error:",error);
-        }
-        }
-        else if (status == "true"){
-          try {
-            const completedtodo = await db.Todo.findAll({
-              where: {
-                completed: true
-              }
-            });
-            const ans = JSON.parse(JSON.stringify(completedtodo));
-            return ans;
-        } catch(error){
-          console.log("error:",error);
-        }
-        }
-        else{
-            try {
-              const completedtodo = await db.Todo.findAll()
-              const ans = JSON.parse(JSON.stringify(completedtodo));
-              return ans;
-          } catch(error){
-            console.log("error:",error);
-          }
-          }
-        };
-        
+      console.log("err", error);
+      throw new ServerError("Error adding task");
+    }
+  }
+  static async getAll(res, req) {
+    const status = req.query.completed;
+    console.log(typeof status);
+    var query;
+
+    if (status === undefined) {
+      query = {}
+    } else {
+      query = {
+        where: {
+          completed: JSON.parse(status),
+        },
+      }
     }
 
-module.exports  =Todolist;
+    try {
+      console.log("error:", query);
+      const tasks = await db.Todo.findAll(query);
+      const ans = JSON.parse(JSON.stringify(tasks));
+      console.log("error:", ans);
+      return ans;
+    } catch (error) {
+      console.log("error:", error);
+    }
+  }
+}
+
+module.exports = Todolist;
